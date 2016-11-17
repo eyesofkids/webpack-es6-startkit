71891573aaf1bf5e1f5ef3afbc67f91bf957f8d6
Eddy Chang <eddy@joomla.com.tw>
2016-11-17 17:40:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-//@flow
+// @flow
 
-const obj = {a:1}
+const obj = { a: 1 }
 
 function func(x: number): any {
   const that = this
 
   setTimeout(
-    function(){
-      console.log(that)
-    }, 2000)
+    () => console.log(that)
+    , 2000)
 
   return x
 }
 
-func.call(obj, '123') //Object {a: 1}
+
+func.call(obj, '123')   // Object {a: 1}
